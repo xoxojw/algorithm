5c8a7e93ae7ba45b5b61654159326c68ef67cf09
Jiwon Park <xoxojw.dev@gmail.com>
2023-06-21 09:45:15 +0900

    [unrated] Title: 홀짝에 따라 다른 값 반환하기, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.js
@@ -0,0 +1,15 @@
+function solution(n) {
+  let result = 0;
+  
+  if (n % 2 === 1) {
+    for (let i = 1; i <= n; i += 2) {
+      result += i;
+    }
+  } else {
+    for (let i = 0; i <= n; i += 2) {
+      result += i**2;
+    }
+  }
+  
+  return result;
+}
