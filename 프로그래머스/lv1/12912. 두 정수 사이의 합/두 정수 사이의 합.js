8ba3498b7f26a85309f975e165fb39bb3a2ba8ca
Jiwon Park <xoxojw.dev@gmail.com>
2023-07-17 18:18:04 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 0.03 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12912. 두 정수 사이의 합/두 정수 사이의 합.js
+++ 프로그래머스/lv1/12912. 두 정수 사이의 합/두 정수 사이의 합.js
@@ -1,13 +1,3 @@
 function solution(a, b) {
-    let sum = 0
-    if (a>b) {
-        for (let i=a; i>=b; i--) {
-            sum +=i
-        }
-    } else {
-        for (let i=a; i<=b; i++) {
-            sum +=i
-    }
-    }
-    return sum;
+    return (a+b)*(Math.abs(a-b)+1)/2
 }
