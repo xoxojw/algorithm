f8763f79f5a8c3cf62a9fd82c00d319fa542e98f
Jiwon Park <xoxojw.dev@gmail.com>
2023-07-21 12:05:29 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 0.09 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
+++ 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
@@ -1,3 +1,5 @@
 function solution(arr) {
-    return arr.length === 1 ? [-1] : arr.filter(v => v !== Math.min(...arr))
+    const min = Math.min(...arr);
+    return arr.length !== 1 ? arr.filter(i => i !== min) : [-1]
+    // return arr.length === 1 ? [-1] : arr.filter(v => v !== Math.min(...arr))
 }
