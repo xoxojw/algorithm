9148df50053f1a37b0e1e64955addaf62611b676
Jiwon Park <xoxojw.dev@gmail.com>
2023-07-24 00:03:50 +0900

    [level 1] Title: 내적, Time: 0.10 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/70128. 내적/내적.js
+++ 프로그래머스/lv1/70128. 내적/내적.js
@@ -1,7 +1,12 @@
 function solution(a, b) {
-    let sum = [];
-    for (let i=0; i<=a.length; i++) {
-        sum.push(a[i]*b[i])
-    }
-    return sum.filter(Number).reduce((acc, cur) => acc+cur)
+// reducer의 네 가지 파라미터
+// array : reduce()를 호출한 배열
+// acc : 이전의 누적 결과 반환
+// cur value : 현재 처리하는 요소
+// cur index : 현재 처리하는 요소의 인덱스
+    
+// 0 => acc의 초기값 설정
+// 초기값을 설정해주지 않으면 초기값이 a[0]으로 자동 설정되어 a[0] + acc로 값이 계산됨
+    
+    return a.reduce((acc, _, i) => acc += a[i]*b[i], 0)
 }
