867287961a4c640dcdbde045f342379e6fe9e794
Jiwon Park <xoxojw.dev@gmail.com>
2023-07-26 09:21:35 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120844. 배열 회전시키기/배열 회전시키기.js
+++ 프로그래머스/lv0/120844. 배열 회전시키기/배열 회전시키기.js
@@ -2,7 +2,8 @@ function solution(numbers, direction) {
     if (direction === "right") {
         numbers.unshift(numbers.pop())
     } else {
-        numbers.shift(numbers.push(numbers[0]))
+        // 이렇게 하면 깔끔..!
+        numbers.push(numbers.shift())
     }
     return numbers;
 }
