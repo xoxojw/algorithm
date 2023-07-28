f7f6b2141128a61ca5b6920ec046a95faa4742fb
Jiwon Park <xoxojw.dev@gmail.com>
2023-07-28 09:41:57 +0900

    [unrated] Title: 가까운 1 찾기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181898. 가까운 1 찾기/가까운 1 찾기.js
+++ 프로그래머스/unrated/181898. 가까운 1 찾기/가까운 1 찾기.js
@@ -4,3 +4,9 @@ const solution = (arr, idx) => {
     }
     return -1
 }
+
+// const solution=(a,i)=>a.indexOf(1,i);
+
+// function solution(arr, idx) {
+    // return arr.findIndex((v, i) => idx <= i && v === 1);
+//}
