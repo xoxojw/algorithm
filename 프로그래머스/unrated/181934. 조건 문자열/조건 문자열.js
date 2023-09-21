c1123f7eb0c4c72e65215a942c81cc57cb3abe84
Jiwon Park <xoxojw.dev@gmail.com>
2023-09-22 01:14:23 +0900

    [unrated] Title: 조건 문자열, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181934. 조건 문자열/조건 문자열.js
@@ -0,0 +1,11 @@
+function solution(ineq, eq, n, m) {
+    if (ineq === ">") {
+        if (eq === "=") {
+            return +(n>=m)
+        } else return +(n>m)
+    } else if (ineq === "<") {
+        if (eq === "=") {
+            return +(n<=m)
+        } else return +(n<m)
+    }
+}
