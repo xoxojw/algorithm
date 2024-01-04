f7122eade07ee2c309dfb54bd18435b7b93e0b6e
Jiwon Park <xoxojw.dev@gmail.com>
2024-01-04 23:17:15 +0900

    [level unrated] Title: 문자 개수 세기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181902. 문자 개수 세기/문자 개수 세기.js
@@ -0,0 +1,8 @@
+function solution(my_string) {
+    let alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    let result = Array(alphabet.length).fill(0);
+    for (let i=0; i<my_string.length; i++) {
+        result[alphabet.indexOf(my_string[i])]++
+    }
+    return result;
+}
