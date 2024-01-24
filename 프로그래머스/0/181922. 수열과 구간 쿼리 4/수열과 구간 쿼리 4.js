503ba52fd54d006413e11af007cefed70aeb99f6
Jiwon Park <xoxojw.dev@gmail.com>
2024-01-24 18:48:38 +0900

    [level 0] Title: 수열과 구간 쿼리 4, Time: 4.54 ms, Memory: 36.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.js
@@ -0,0 +1,11 @@
+function solution(arr, queries) {
+    for (const query of queries) {
+        const [s, e, k] = query;
+        
+        for (let i = s; i <= e; i++) {
+            if (i%k === 0) arr[i]++;
+        }
+    }
+    
+    return arr;
+}
