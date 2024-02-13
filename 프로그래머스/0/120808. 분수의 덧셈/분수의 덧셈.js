5d99ba15a5c3108d144b477704660bd9cd4dfa43
Jiwon Park <xoxojw.dev@gmail.com>
2024-02-13 23:27:55 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.js
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.js
@@ -1,9 +1,11 @@
 function solution(numer1, denom1, numer2, denom2) {
-    let answer = [(numer1 * denom2) + (numer2 * denom1), denom1 * denom2];
+    let [numer, denom] = [(numer1 * denom2) + (numer2 * denom1), denom1 * denom2];
+    
     const getGCD = (a, b) => {
         if (b === 0) return a;
         return getGCD(b, a % b);
-    }
-    let gcd = getGCD(answer[0], answer[1])
-    return [answer[0]/gcd, answer[1]/gcd];
+    };
+    const gcd = getGCD(numer, denom);
+    
+    return [numer/gcd, denom/gcd];
 }
