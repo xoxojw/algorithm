d33f67dfcb8b5a36342e3068ac66d64134d41ccb
Jiwon Park <xoxojw.dev@gmail.com>
2024-02-14 12:18:04 +0900

    [level 0] Title: 코드 처리하기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.js
@@ -0,0 +1,18 @@
+function solution(code) {
+    let ret = '';
+    let mode = 0;
+    
+    for (let i = 0; i < code.length; i++) {
+        // 모드 전환
+        if (code[i] === "1") {
+            mode === 0 ? mode = 1 : mode = 0
+        }
+        
+        if (code[i] !== "1") {
+            if (mode === 0 && i%2 === 0) ret += code[i]
+            if (mode === 1 && i%2 === 1) ret += code[i]
+        }
+    }
+    
+    return ret ? ret : "EMPTY";
+}
