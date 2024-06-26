7f8fc10d5a61b357599963ac9ddc80f64cf9e0a8
Jiwon Park <xoxojw.dev@gmail.com>
2024-06-26 23:36:26 +0900

    [level 1] Title: 명예의 전당 (1), Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.js
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.js
@@ -1,12 +1,30 @@
-const solution = (k, score) => {
-    let hallOfFame = [];
-    let LowestScores = [];
+// const solution = (k, score) => {
+//     let hallOfFame = [];
+//     let LowestScores = [];
     
-    for (let todayScore of score) {
-        hallOfFame.push(todayScore);
-        hallOfFame.sort((a, b) => b - a).splice(k);
-        LowestScores.push(hallOfFame[hallOfFame.length-1]);
-    }
+//     for (let todayScore of score) {
+//         hallOfFame.push(todayScore);
+//         hallOfFame.sort((a, b) => b - a).splice(k);
+//         LowestScores.push(hallOfFame[hallOfFame.length-1]);
+//     }
     
-    return LowestScores;
+//     return LowestScores;
+// }
+
+function solution(k, score) {
+    const stack = []
+    return score.reduce((a,c) => {
+        if(stack.length < k) {
+            stack.push(c)
+            stack.sort((a,b) => a - b)
+        }
+        else {
+            stack.push(c)
+            stack.sort((a,b) => a - b)
+            stack.shift()
+        }
+        a.push(stack[0])
+        return a
+    },[])
 }
+
