4bd0a79197b19ece6074ac22674ff4db5f638af3
Jiwon Park <xoxojw.dev@gmail.com>
2024-09-22 21:36:42 +0900

    Time: 57 ms (27.45%), Space: 48.9 MB (66.55%) - LeetHub

---
+++ leetcode/0035-search-insert-position/0035-search-insert-position.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+    let left = 0;
+    let right = nums.length - 1;
+
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+
+        if (nums[mid] === target) return mid;
+        if (nums[mid] < target) left = mid + 1;
+        if (nums[mid] > target) right = mid - 1;
+    }
+
+    return left;
+};
